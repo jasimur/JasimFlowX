51b2fcba47890ba6cac6bbbe3c064fbdda173f9f
jasimur <jasimurrahman01@gmail.com>
2026-09-14 13:42:33 +0600

    Add Firebase auth public configuration

---
+++ lib/firebase-config.ts
@@ -0,0 +1,5 @@
+export const FIREBASE_PROJECT_ID = "trade-journal-26";
+export const FIREBASE_API_KEY = "AIzaSyCfrhv9C4wotZflOwHyMlvJ20xHo8ou1aY";
+export const FIREBASE_AUTH_DOMAIN = "trade-journal-26.firebaseapp.com";
+
+export const FIREBASE_SIGN_IN_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_API_KEY}`;
